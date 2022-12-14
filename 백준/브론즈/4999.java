b6496e6d5e15e2d302596af95cb46144efd546c0
lee02g29 <lee02g29@naver.com>
2022-12-15 03:50:44 +0900

    백준 브론즈5 - 4999번, 아!
    백준 브론즈5 - 4999번, 아!

---
+++ 백준/브론즈/4999.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+    		Scanner sc = new Scanner(System.in);
+
+        	String f = sc.nextLine();
+        	String s = sc.nextLine();
+        	
+        	if(f.length() >= s.length()) {
+        		System.out.println("go");
+        	} else {
+        		System.out.println("no");
+        	}
+
+    }
+}
