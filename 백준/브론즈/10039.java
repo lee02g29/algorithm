ac9307973490c032af65c9dc3d85bd39afa5599e
lee02g29 <lee02g29@naver.com>
2022-12-17 04:21:48 +0900

    백준 브론즈4 - 10039번, 평균 점수
    백준 브론즈4 - 10039번, 평균 점수

---
+++ 백준/브론즈/10039.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+    		Scanner sc = new Scanner(System.in);
+    		
+    		int sum = 0;
+    		for(int i = 0; i < 5; i++) {
+    			int score = sc.nextInt();
+    			if(score < 40) score = 40;
+    			sum = sum + score;  			
+    		}
+    		System.out.println(sum / 5);			
+    }
+}
