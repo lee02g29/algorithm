4f4b4403910d0e238c0420beea26b32f7a289d7a
lee02g29 <lee02g29@naver.com>
2023-01-24 06:18:38 +0900

    백준 실버5 - 11651번, 좌표 정렬하기 2
    백준 실버5 - 11651번, 좌표 정렬하기 2

--- 백준/실버/11651.java
+++ 백준/실버/11651.java
@@ -12,7 +12,6 @@ class Main  {
     	
     	int n = Integer.parseInt( bf.readLine() );
     	ArrayList<pair> coord = new ArrayList<>();
-    	int[] order = new int[n];
     	
     	for(int i = 0; i < n; i++) {
     		String[] nums = bf.readLine().split(" ");
@@ -31,7 +30,7 @@ class Main  {
     	bw.close();
     }   
     
-    static class pair implements Comparable<pair>{
+    static class pair implements Comparable<pair>{ // 좌표를 저장할 pair 클래스
     	int x, y;
     	pair(int x, int y) {
     		this.x = x;
@@ -39,11 +38,15 @@ class Main  {
     	}
     	
     	@Override
-    	public int compareTo(pair p1) {
-        	if(this.y == p1.y) {
+    	public int compareTo(pair p1) { // y값을 오름차순으로 정렬, 같다면 x의 값에 따라 정렬
+        	if(this.y == p1.y) { // y의 값이 같다면
         		return this.x - p1.x;
+                // 함수를 호출한 좌표의 x 값 > 매개변수로 입력된 좌표의 x 값 이라면
+                // 객체의 자리를 자꾼다.
         	} else
         		return this.y - p1.y;
+                // 함수를 호출한 좌표의 y 값 > 매개변수로 입력된 좌표의 y 값 이라면
+                // 객체의 자리를 자꾼다.
         }
     }    
 }
