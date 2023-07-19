c530fd49a7b323f0c814ce25c8b5dddee230b3ba
lee02g29 <lee02g29@naver.com>
2023-07-19 16:44:01 +0900

    백준 브론즈5 - 25083번, 새싹
    백준 브론즈5 - 25083번, 새싹

---
+++ 백준/브론즈/25083.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("         ,r'\"7" + "\n");
+		bw.write("r`-_   ,'  ,/" + "\n");
+		bw.write(" \\. \". L_r'" + "\n");
+		bw.write("   `~\\/" +"\n");
+		bw.write("      |" + "\n");
+		bw.write("      |" + "\n");
+		
+		bw.flush();
+		bw.close();
+
+	}
+}
