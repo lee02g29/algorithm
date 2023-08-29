5e0d8c9ccc37e1306441386aa370eaa0294db2b1
lee02g29 <lee02g29@naver.com>
2023-08-29 14:56:35 +0900

    백준 실버3 - 1463번, 1로 만들기, 두 번째 방법
    백준 실버3 - 1463번, 1로 만들기, 두 번째 방법

--- 백준/실버/1463-2.java
+++ 백준/실버/1463-2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Assignment {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
